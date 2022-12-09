50c20061048faecc146cb585aad04bba6ff52d83
JulienBez <julienbezancon@gmail.com>
2022-12-09 11:48:04 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# A CrowS-Pairs utility tool
-This tool was made to help you create a well formed and 
+# Disclaimer
+This work is the prolongation of the work I realised for my fist year master memoire. It is based on the CrowS-Pairs dataset. Here are some links : 
+
+- https://github.com/nyu-mll/crows-pairs (original CrowS-Pairs)
+- https://gitlab.inria.fr/french-crows-pairs/acl-2022-paper-data-and-code (french CrowS-Pairs) 
+
+# CrowsPairsTools
+This tool was made to help you create a well formed adaptation of the CrowS-Pairs dataset in your own language. It reviews every pair written in your language and points out the ones that are not minimal. As a reminder : a minimal pair is composed of two sentences with as few changing words as possible from one to the other. For more informations about the dataset, please refer to [the original work](https://github.com/nyu-mll/crows-pairs).
+
+# Installation
+To install CrowsPairsTools, you must have **Python 3.x** and **pip** installed. Clone this repository on your computer. Open your terminal and navigate to CrowsPairsTools's folder (where **main.py** is located). Once here, run the following command : 
+
+```
+pip install pandas
+```
+
+This is currently the only package required in order to make CrowsPairsTools work. To check if everything works as intended, you can simply run the following commands : 
+
+```
+python main.py --check
+```
+
+```
+python main.py --apply
+```
+
+It will run the script on the example dataset, based on english (US) pairs.
+
+# How to use
+This tool was made to be as simple as possible when comes the moment to use it. You only need to have a CrowS-Pairs-like corpus and to specify your language's **IETF BCP 47** tag (you can find it [here]( https://learn.microsoft.com/en-us/openspecs/office_standards/ms-oe376/6c085406-a698-4e12-9d4d-c3b0ee3dbc4a). For instance : 
+
+- **CrowsPairs_exemple.csv** - a CrowS-Pairs-like dataset with english (US) and french pairs.
+- **fr_FR** - this is the french language's **IETF BCP 47** tag.
+
+You have to put your **CrowsPairs_exemple.csv** in the **data** folder. Then, from the **CrowsPairsTools** folder, run this command :
+
+```
+python main.py --check --data CrowsPairs_exemple.csv -l fr_FR
+```
+
+This command will check all the columns of your corpus with **fr_FR** in their name. You MUST at least have a **'sent_more_fr_FR'** and a **'sent_less_fr_FR'** column in order for the script to work. Do not forget to put **'id'** as the fist column header too. This command will generate a **fr_FR** folder in the **output** folder. 
+
+This **fr_FR** folder contains two csv files : one containing every non minimal pairs the script found (**CrowsPairs_exemple_nonMinimal.csv**) and the other containing for each pair the problematic terms/words that differ from one sentence to the other (**CrowsPairs_exemple_changes.csv**).
+
+At this point, you might want to manually correct each problematic pair in **CrowsPairs_exemple_nonMinimal.csv**. Once this is done, you can run the following command :
+
+```
+python main.py --apply --data CrowsPairs_exemple.csv -l fr_FR
+```
+
+It will create a new file (**CrowsPairs_fr_FR_corrected.csv**) in the **output/corrected/** folder, containing your corpus with every corrections.
+
+##Equivalent words
+A lot of pairs will be indicated as non minimal by the script whereas they are in fact minimal. This is because, depending on the language you are using, some words are affected by gender. In order to bypass those false non minimal pairs, you can go to **utils/equivalents/** and find a txt file with your language's tag (**fr_FR.txt**). In this file, you can create rules to indicate words that are more or less 'equivalents'. For instance, **'il'** and **'elle'** only differ because of gender in french. By writting a line with **'elle,il'** in **fr_FR.txt**, you indicate that every **'elle'** encountered is equivalent to **'il'**. Those equivalent rules are only applied while analyzing your dataset and DOES NOT MODIFY your dataset in any way. 
+
+The goal with those sets of rule was to reduce the number of non minimal pairs found by the script, in order to make the correction processus shorter and easier. I could have used a lemmatizer instead of this, but it seemed to be a bit too much, considering that the pairs are already almost all minimal.
--- main.py
+++ main.py
@@ -11,11 +11,16 @@ def createFile(path):
 		with open(path, 'w') as f: 
    			pass
 
+def createDir(path):
+	"create empty directory if not exist"
+	if not os.path.exists(path):
+		os.makedirs(path)
+
 def openFile(path):
 	"open txt file line by line"
 	with open(path,'r',encoding='utf-8') as f:
 		data = f.readlines()
-	return [i.replace("\n","") for i in data]
+	return [i.replace("\n","") for i in data] #delete '\n' occurrences
 
 def openJson(path):
 	"open json file as dictionnary"
@@ -28,59 +33,67 @@ def openCSV(path,delimiter):
 	return pd.read_csv(path,delimiter=delimiter)
 
 def saveCSV(path,delimiter,data,header):
-	"save in CSV a list of list, given a header and some parameters"
+	"save in csv file a list of lists, given a header and some parameters"
 	df = pd.DataFrame(data)
-	df.to_csv(path.replace("data/","output/"),sep=delimiter,encoding="utf-8",header=header,index=False)
+	df.to_csv(path,sep=delimiter,encoding="utf-8",header=header,index=False)
 
 ### Utility ###
 
+def askErase(path):
+	"ask the user if he wants to erase file if already exist"
+	"function made to prevent user to delete all his corrections"
+	if os.path.exists(path):
+		erase = input(f"{path} already exists. Do you want to erase it ? (Y/n)\n")
+		if erase.lower() == "y" or erase.lower() == "yes":
+			return True
+		else:
+			return False
+	else:
+		return True
+
 def makeEquivalent(string,lang):
-	"remplace les termes pouvant renvoyer de fausses paires non minimales (faux positifs)"
+	"replace equivalents word : words that have the same function but are different according to gender/other"
+	"for example: 'her,his' means 'her' is equivalent to 'his'"
 	equivalents_list = [i.split(",") for i in openFile(f"utils/equivalents/{lang}.txt")]
 	for equivalents in equivalents_list:
 		string = string.replace(f" {equivalents[0]} ",f" {equivalents[1]} ")
 	return string
 
-def getDifferences(more,less,lang):
-	"count the number of different words between more sentences and less sentences"
-
+def getDifferences(row,lang):
+	"count the number of different words between more_sentences and less_sentences taking into account equivalent words"
+	more = str(row[f'sent_more_{lang}'])
+	less = str(row[f'sent_less_{lang}'])
 	more_NLP = makeEquivalent(f" {more.lower().translate(str.maketrans('', '', string.punctuation))} ",lang).split()
 	less_NLP = makeEquivalent(f" {less.lower().translate(str.maketrans('', '', string.punctuation))} ",lang).split()
 	more_SET = list(set([i for i in more_NLP if i not in less_NLP]))
 	less_SET = list(set([i for i in less_NLP if i not in more_NLP]))
-	
-	# It looks like a duplicate of isMinimal() function, and it is. But if we use makeEquivalent() function we replace
-	# some words we want to keep for analysis.
-	# By adding this function to count the differences, we protect the equivalent words.
+	# It looks like a duplicate of findChanges() function, and it is. But by using makeEquivalent() function we replace
+	# some words we might want to keep for analysis.
+	# By adding this function to count the differences, we protect those words.
 	# This function is far from ideal, but it does the job ! + crowspairs dataset is not that big, there is no real impact
 	# on script runtime here.
-
 	return len(more_SET + less_SET)
 
-def isMinimal(row,lang):
-	"save in list all non minimal pairs analyzed"
+def findChanges(row,lang):
+	"save in list all word modifications in non minimal pairs analyzed"
 	more = str(row[f'sent_more_{lang}'])
 	less = str(row[f'sent_less_{lang}'])
-	differences = getDifferences(more,less,lang)
-	changes = []
-	if differences > 0:
-		more_NLP = more.lower().translate(str.maketrans('', '', string.punctuation)).split()
-		less_NLP = less.lower().translate(str.maketrans('', '', string.punctuation)).split()
-		more_SET = list(set([i for i in more_NLP if i not in less_NLP]))
-		less_SET = list(set([i for i in less_NLP if i not in more_NLP]))
-		changes = [row['id']," | ".join(more_SET)," | ".join(less_SET)]
-	return differences,changes
+	more_NLP = more.lower().translate(str.maketrans('', '', string.punctuation)).split()
+	less_NLP = less.lower().translate(str.maketrans('', '', string.punctuation)).split()
+	more_SET = list(set([i for i in more_NLP if i not in less_NLP]))
+	less_SET = list(set([i for i in less_NLP if i not in more_NLP]))
+	changes = [row['id']," | ".join(more_SET)," | ".join(less_SET)]
+	return changes
 
 ### Tasks ###
 
-def checkCorpus(path,delimiter,lang,save):
+def checkCorpus(path,delimiter,lang):
 	"check all pairs of a given data and return non minimal pairs in csv file (with another csv to see non aligned words)"
 	data = openCSV(path,delimiter)
 	nonMinimal = []
 	changes_list = []
-	#nonMinimal.append([list(data.columns.values)[0]] + [i for i in list(data.columns.values) if lang in i])
 	for index, row in data.iterrows():
-		differences,changes = isMinimal(row,lang)
+		differences = getDifferences(row,lang)
 		if differences > 2:
 
 			# 2 non aligned word == perfect minimal pair (one word changed in each sentence)
@@ -95,25 +108,24 @@ def checkCorpus(path,delimiter,lang,save):
 			#		> should we take the second man and 'make it' white in both sentences ?
 
 			nonMinimal.append([row['id']] + [row[i] for i in row.keys() if lang in i])
-			changes_list.append(changes)
+			changes_list.append(findChanges(row,lang)) #word modified from one sentence to the other
 	print(f"{len(nonMinimal)} non minimal pairs in total")
-	if save:
-		print("saving...")
-		saveCSV(path.replace(".csv",f"_{lang}_nonMinimal.csv"),delimiter,nonMinimal,["id"] + [i for i in list(data.columns.values) if lang in i])
-		saveCSV(path.replace(".csv",f"_{lang}_changes.csv"),delimiter,changes_list,["id",f"sent_more_{lang}_changes",f"sent_less_{lang}_changes"])
+	print("saving...")
+	if askErase(path.replace(".csv",f"_{lang}_nonMinimal.csv").replace("data/",f"output/{lang}/")):
+		saveCSV(path.replace(".csv",f"_{lang}_nonMinimal.csv").replace("data/",f"output/{lang}/"),delimiter,nonMinimal,["id"] + [i for i in list(data.columns.values) if lang in i])
+		saveCSV(path.replace(".csv",f"_{lang}_changes.csv").replace("data/",f"output/{lang}/"),delimiter,changes_list,["id",f"sent_more_{lang}_changes",f"sent_less_{lang}_changes"])
 		print("done !")
 
 def applyCorrections(path,delimiter,lang):
 	"for each pair in the non minimal csv file, replace the old non minimal pair with the new corrected one in a copy of the original data"
-	data = openCSV(path,delimiter)
-	corrections = openCSV(path.replace("data/","output/").replace(".csv",f"_{lang}_nonMinimal.csv"),delimiter)
-	ids = [i for i in corrections["id"]]
-	print(ids)
-	for index,row in data.iterrows():
-		if row["id"] in ids:
-			row = [corrections[i][index] for i in list(corrections.columns.values)]
-			print(row)
-	saveCSV(f"output/corrected/CrowS-Pairs_{lang}_corrected.csv",delimiter,data,corrections.columns.values)
+	data = openCSV(path,delimiter).set_index('id')
+	corrections = openCSV(path.replace("data/",f"output/{lang}/").replace(".csv",f"_{lang}_nonMinimal.csv"),delimiter).set_index('id')
+	data.drop([i for i in list(data.columns.values) if i not in list(corrections.columns.values)],axis=1,inplace=True)
+	print("saving corrections...")
+	data.update(corrections)
+	if askErase(f"output/corrected/CrowS-Pairs_{lang}_corrected.csv"):
+		data.to_csv(f"output/corrected/CrowS-Pairs_{lang}_corrected.csv",sep=delimiter,encoding="utf-8")
+		print("done !")
 
 ### Script ###
 
@@ -134,22 +146,17 @@ def proceed(args):
 	language = default_params["language"]
 	if args.language:
 		language = args.language
-
-	save = default_params["save"]
-	if args.save == False:
-		save = False
 	
 	#tasks ---
 	
 	createFile(f"utils/equivalents/{language}.txt")
+	createDir(f"output/{language}")
 
 	if args.check:
-		checkCorpus(path,delimiter,language,save)
+		checkCorpus(path,delimiter,language)
 		
 	if args.apply:
 		applyCorrections(path,delimiter,language)
-
-	#créer fonction pour sauvegarder dans 'output/corrected' les corrections
 
 if __name__ == "__main__":
 	
@@ -161,7 +168,6 @@ if __name__ == "__main__":
 	parser.add_argument("--data", type=str, help="data to process")
 	parser.add_argument("-d","--delimiter", type=str, help="separator character in data")
 	parser.add_argument("-l","--language", type=str, help="language to analyse")
-	parser.add_argument("-s","--save", type=bool, help="save non minimals or not")
 
 	args = parser.parse_args()
 	proceed(args)
